8c922899b6079e669498aa973ae3c83d905d7f3b
PauloNobre <paulinho140@gmail.com>
2020-06-13 19:55:52 -0300

    Criando Service de product

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ProductService } from './../product.service';
+
 
 @Component({
   selector: 'app-product-create',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private productService: ProductService,
+    private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  createProduct(): void {
+    this.productService.showMessage('Produto Cadastrado!');
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
   }
 
 }
